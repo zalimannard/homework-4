741933accee10e8ba8d244d092cd79d185363788
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-10-20 09:28:23 +0300

    chore: Add table creation

--- simulation-of-operations/lab-2/src/main/java/ru/zalimannard/Main.java
+++ simulation-of-operations/lab-2/src/main/java/ru/zalimannard/Main.java
@@ -3,10 +3,42 @@ package ru.zalimannard;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+
 public class Main {
     private static Logger logger = LogManager.getRootLogger();
 
     public static void main(String[] args) {
-        logger.info("Hello world");
+        ArrayList<ArrayList<String>> table = readTable();
+        printTable(table);
+    }
+
+    private static ArrayList<ArrayList<String>> readTable() {
+        CsvFileReader csvFileReader = new CsvFileReader();
+
+        ArrayList<ArrayList<String>> table = csvFileReader.read("transportation-cost.csv");
+        ArrayList<ArrayList<String>> consumers = csvFileReader.read("consumers.csv");
+        ArrayList<ArrayList<String>> suppliers = csvFileReader.read("suppliers.csv");
+
+        table.get(0).add("");
+        for (int i = 1; i < table.size(); ++i) {
+            table.get(i).add(suppliers.get(1).get(i - 1));
+        }
+
+        ArrayList<String> lastLine = new ArrayList<>();
+        lastLine.add("");
+        for (int i = 0; i < consumers.get(1).size(); ++i) {
+            lastLine.add(consumers.get(1).get(i));
+        }
+        lastLine.add("");
+        table.add(lastLine);
+
+        return table;
+    }
+
+    private static void printTable(ArrayList<ArrayList<String>> table) {
+        for (ArrayList<String> line : table) {
+            System.out.println(line);
+        }
     }
 }
