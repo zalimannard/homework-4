37d3b2b008493a166db22a4c53fd50c46095ae52
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-10-28 00:55:34 +0300

    feat: Сделать простейший запрос

--- artificial-intelligence-systems/lab-1/main.py
+++ artificial-intelligence-systems/lab-1/main.py
@@ -7,9 +7,17 @@ f = open("token.txt")
 token = f.readline()
 bot = telebot.TeleBot(token)
 
+model_config = read_json('squad_ru_bert_infer.json')
+model = build_model(model_config, download=True)
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Помощь")
+
+@bot.message_handler(commands=['q'])
+def send_welcome(message):
+	bot.reply_to(message, model(['Владимир Путин президент Российской Федерации. Ему 70 лет'], ['Кто такой Владимир Путин?']))
+	bot.reply_to(message, model(['Владимир Путин президент Российской Федерации. Ему 70 лет'], ['Сколько лет Путину?']))
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
