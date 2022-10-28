e702ef9b6bf499f31bb5b2c2222365d48a7c7a6c
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-10-28 04:03:43 +0300

    feat: Рабочая программа для задания
    Есть задание текста, получение сообщений и ответ на них исходя из
    контекста. Работает не очень хорошо, но это уже проблемы библиотеки.

--- artificial-intelligence-systems/lab-1/main.py
+++ artificial-intelligence-systems/lab-1/main.py
@@ -3,24 +3,28 @@ import sys
 from deeppavlov import build_model, configs
 from deeppavlov.core.common.file import read_json
 
-f = open("token.txt")
-token = f.readline()
-bot = telebot.TeleBot(token)
-
-model_config = read_json('squad_ru_bert_infer.json')
+model_config = read_json("squad_ru_bert_infer.json")
 model = build_model(model_config, download=True)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Помощь")
+tokenFile = open("token.txt")
+token = tokenFile.readline()
 
-@bot.message_handler(commands=['q'])
+contextFile = open("text-about-bashkortostan.txt")
+context = contextFile.read()
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
-	bot.reply_to(message, model(['Владимир Путин президент Российской Федерации. Ему 70 лет'], ['Кто такой Владимир Путин?']))
-	bot.reply_to(message, model(['Владимир Путин президент Российской Федерации. Ему 70 лет'], ['Сколько лет Путину?']))
+    bot.reply_to(message, "Сәләм. Башҡортостан буйынса һорауҙар бирә алаһың.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+	messageText = message.text
+	answer = model([context], [messageText])
+	if answer[0][0].strip():
+		bot.reply_to(message, answer)
+	else:
+		bot.reply_to(message, "Мин һине аңламаным")
 
 bot.infinity_polling()
