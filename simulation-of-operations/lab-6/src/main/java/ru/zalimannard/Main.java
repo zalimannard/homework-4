1ed2718ce97237f34a3f6990dd34d3d99e8b1614
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-02-16 22:52:56 +0300

    feat: Класс для Матрицы

---
+++ simulation-of-operations/lab-6/src/main/java/ru/zalimannard/Main.java
@@ -0,0 +1,8 @@
+package ru.zalimannard;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1:");
+        Matrix<Double> table = new Matrix<>(4, 3, 0.0);
+    }
+}
