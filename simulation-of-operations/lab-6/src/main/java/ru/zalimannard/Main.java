bc25c6d396b937f2bbb4c4f9fe6ae0a068882e40
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-02-26 15:05:05 +0300

    feat(soo-6): Первое задание сделано

--- simulation-of-operations/lab-6/src/main/java/ru/zalimannard/Main.java
+++ simulation-of-operations/lab-6/src/main/java/ru/zalimannard/Main.java
@@ -2,7 +2,83 @@ package ru.zalimannard;
 
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
+
+    private static void task1() {
         System.out.println("Задание 1:");
-        Matrix<Double> table = new Matrix<>(4, 3, 0.0);
+        Matrix<Double> matrix = new Matrix<>(4, 3, 0.0);
+        matrix.set(1,1, 3.0); matrix.set(2, 1, -5.0); matrix.set(3, 1, 1.0); matrix.set(4, 1, -2.0);
+        matrix.set(1,2, 4.0); matrix.set(2, 2, 2.0); matrix.set(3, 2, -4.0); matrix.set(4, 2, 3.0);
+        matrix.set(1,3, 2.0); matrix.set(2, 3, -3.0); matrix.set(3, 3, 5.0); matrix.set(4, 3, 4.0);
+        System.out.println("Изначальная матрица:");
+        System.out.println(matrix);
+
+        Matrix<Double> matrixWithMinAndMax = new Matrix<>(matrix.getWidth() + 1,  matrix.getHeight() + 1, 0.0);
+        copy(matrix, matrixWithMinAndMax);
+        for (int x = 1; x <= matrix.getWidth(); ++x) {
+            matrixWithMinAndMax.set(x, matrixWithMinAndMax.getHeight(), maxInColumn(matrix, x));
+        }
+        for (int y = 1; y <= matrix.getHeight(); ++y) {
+            matrixWithMinAndMax.set(matrixWithMinAndMax.getWidth(), y, minInRow(matrix, y));
+        }
+        matrixWithMinAndMax.set(matrixWithMinAndMax.getWidth(), matrixWithMinAndMax.getHeight(), null);
+        System.out.println("Найденные минимумы и максимумы:");
+        System.out.println(matrixWithMinAndMax);
+
+        Double maxInLastColumn = maxInColumn(matrixWithMinAndMax, matrixWithMinAndMax.getWidth());
+        System.out.println("Нижняя цена игры (максимум из нового столбца): " + maxInLastColumn);
+        Double minInLastRow = minInRow(matrixWithMinAndMax, matrixWithMinAndMax.getHeight());
+        System.out.println("Верхняя цена игры (минимум из новой строки): " + minInLastRow);
+
+        if (maxInLastColumn.equals(minInLastRow)) {
+            System.out.println("Седловая точка есть");
+            for (int x = 1; x <= matrix.getWidth(); ++x) {
+                for (int y = 1; y <= matrix.getHeight(); ++y) {
+                    if (matrixWithMinAndMax.get(x, y).equals(maxInLastColumn)) {
+                        System.out.println("Её координаты (" + x + "; " + y + ") и значение в ней " + maxInLastColumn);
+                        return;
+                    }
+                }
+            }
+        } else {
+            System.out.println("Седловой точки нет");
+        }
+    }
+
+    private static void copy(Matrix<Double> matrix, Matrix<Double> matrixWithMinAndMax) {
+        for (int x = 1; x <= matrix.getWidth(); ++x) {
+            for (int y = 1; y <= matrix.getHeight(); ++y) {
+                matrixWithMinAndMax.set(x, y, matrix.get(x, y));
+            }
+        }
+    }
+
+    private static double maxInColumn(Matrix<Double> matrix, int x) {
+        double max = matrix.get(x, 1);
+        for (int y = 1; y < matrix.getWidth(); ++y) {
+            // Чтобы не учитывать нижнюю правую ячейку
+            if (matrix.get(x, y) == null) {
+                continue;
+            }
+            if (matrix.get(x, y) > max) {
+                max = matrix.get(x, y);
+            }
+        }
+        return max;
+    }
+
+    private static double minInRow(Matrix<Double> matrix, int y) {
+        double min = matrix.get(1, y);
+        for (int x = 1; x < matrix.getWidth(); ++x) {
+            // Чтобы не учитывать нижнюю правую ячейку
+            if (matrix.get(x, y) == null) {
+                continue;
+            }
+            if (matrix.get(x, y) < min) {
+                min = matrix.get(x, y);
+            }
+        }
+        return min;
     }
 }
--- simulation-of-operations/lab-6/src/main/java/ru/zalimannard/Matrix.java
+++ simulation-of-operations/lab-6/src/main/java/ru/zalimannard/Matrix.java
@@ -8,9 +8,9 @@ public class Matrix<T> {
     private List<List<T>> content = new ArrayList<>();
 
     public Matrix(int width, int height, T initialValue) {
-        for (int y = 0; y < height; ++y) {
+        for (int y = 0; y < height + 1; ++y) {
             ArrayList<T> row = new ArrayList<>();
-            for (int x = 0; x < width; ++x) {
+            for (int x = 0; x < width + 1; ++x) {
                 row.add(initialValue);
             }
             content.add(row);
@@ -18,35 +18,36 @@ public class Matrix<T> {
     }
 
     public T get(int x, int y) {
-        return content.get(y).get(x);
+        return content.get(y - 1).get(x - 1);
     }
 
     public void set(int x, int y, T value) {
-        content.get(y).set(x, value);
+        content.get(y - 1).set(x - 1, value);
     }
 
     public int getWidth() {
-        if (getHeight() > 0) {
-            return content.get(0).size();
-        }
-        return 0;
+        return content.get(0).size() - 1;
     }
 
     public int getHeight() {
-        return content.size();
+        return content.size() - 1;
     }
 
     @Override
     public String toString() {
         String answer = "";
-        int cellLength = Math.max(5, getMaxLength());
+        int cellLength = Math.max(4, getMaxLength());
         String horizontalLine = String.join("", Collections.nCopies((cellLength + 3) * getWidth() + 1, "~"));
 
-        for (int y = 0; y < getHeight(); ++y) {
-            answer += horizontalLine + "\n";
-            for (int x = 0; x < getWidth(); ++x) {
-                String spaces = String.join("", Collections.nCopies(cellLength - get(x, y).toString().length(), " "));
-                answer += "| " + spaces + get(x, y).toString() + " ";
+        answer += horizontalLine + "\n";
+        for (int y = 1; y <= getHeight(); ++y) {
+            for (int x = 1; x <= getWidth(); ++x) {
+                String valueAsString = "";
+                if (get(x, y) != null) {
+                    valueAsString = get(x, y).toString();
+                }
+                String spaces = String.join("", Collections.nCopies(cellLength - valueAsString.length(), " "));
+                answer += "| " + spaces + valueAsString + " ";
             }
             answer += "|\n";
         }
@@ -57,8 +58,11 @@ public class Matrix<T> {
 
     private int getMaxLength() {
         int maxLength = 0;
-        for (int x = 0; x < getWidth(); ++x) {
-            for (int y = 0; y < getHeight(); ++y) {
+        for (int x = 1; x <= getWidth(); ++x) {
+            for (int y = 1; y <= getHeight(); ++y) {
+                if (get(x, y) == null) {
+                    continue;
+                }
                 if (get(x, y).toString().length() > maxLength) {
                     maxLength = get(x, y).toString().length();
                 }
