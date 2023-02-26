8883ecbf221baa8263a3d348bd6207fde483cc29
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-02-26 15:16:05 +0300

    feat(soo-6): Подготовка к двум следующим заданиям

--- simulation-of-operations/lab-6/src/main/java/ru/zalimannard/Main.java
+++ simulation-of-operations/lab-6/src/main/java/ru/zalimannard/Main.java
@@ -2,15 +2,26 @@ package ru.zalimannard;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
+        Matrix<Double> matrix1 = new Matrix<>(4, 3, 0.0);
+        matrix1.set(1,1, 3.0); matrix1.set(2, 1, -5.0); matrix1.set(3, 1, 1.0); matrix1.set(4, 1, -2.0);
+        matrix1.set(1,2, 4.0); matrix1.set(2, 2, 2.0); matrix1.set(3, 2, -4.0); matrix1.set(4, 2, 3.0);
+        matrix1.set(1,3, 2.0); matrix1.set(2, 3, -3.0); matrix1.set(3, 3, 5.0); matrix1.set(4, 3, 4.0);
+        task1(matrix1);
+        System.out.println("\n\n\n");
+
+        Matrix<Double> matrix2 = new Matrix<>(4, 3, 0.0);
+        matrix2.set(1,1, -2.0); matrix2.set(2, 1, +8.0); matrix2.set(3, 1, +5.0); matrix2.set(4, 1, +7.0);
+        matrix2.set(1,2, -3.0); matrix2.set(2, 2, +9.0); matrix2.set(3, 2, +3.0); matrix2.set(4, 2, +5.0);
+        matrix2.set(1,3, +8.0); matrix2.set(2, 3, -1.0); matrix2.set(3, 3, +9.0); matrix2.set(4, 3, +3.0);
+
+        Matrix matrix3 = task2(matrix2);
+        System.out.println("\n\n\n");
+
+        // task3(matrix3);
     }
 
-    private static void task1() {
+    private static void task1(Matrix matrix) {
         System.out.println("Задание 1:");
-        Matrix<Double> matrix = new Matrix<>(4, 3, 0.0);
-        matrix.set(1,1, 3.0); matrix.set(2, 1, -5.0); matrix.set(3, 1, 1.0); matrix.set(4, 1, -2.0);
-        matrix.set(1,2, 4.0); matrix.set(2, 2, 2.0); matrix.set(3, 2, -4.0); matrix.set(4, 2, 3.0);
-        matrix.set(1,3, 2.0); matrix.set(2, 3, -3.0); matrix.set(3, 3, 5.0); matrix.set(4, 3, 4.0);
         System.out.println("Изначальная матрица:");
         System.out.println(matrix);
 
@@ -44,6 +55,20 @@ public class Main {
         } else {
             System.out.println("Седловой точки нет");
         }
+    }
+
+    private static Matrix task2(Matrix matrix) {
+        System.out.println("Задание 2:");
+        System.out.println("Изначальная матрица:");
+        System.out.println(matrix);
+
+        return matrix;
+    }
+
+    private static void task3(Matrix matrix) {
+        System.out.println("Задание 3:");
+        System.out.println("Изначальная матрица:");
+        System.out.println(matrix);
     }
 
     private static void copy(Matrix<Double> a, Matrix<Double> b) {
